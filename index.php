abb19ebe0d1e8ab34611e793329a402fd39f5dad
dblancoformacion <dblancoformacion@gmail.com>
2024-10-01 09:52:09 +0200

    Login simple

--- index.php
+++ index.php
@@ -5,3 +5,20 @@
 </style>
 
 Hola <b>Mundo</b>!!
+
+<?php
+
+if(isset($_GET['passwd']) and $_GET['passwd']=='1234'){
+
+	echo 'Esto es secreto';
+
+}
+else{
+	echo '<form>'
+		.'<input name="passwd" type="password">'
+		.'<button>Envía contraseña</button>
+		.</form>';
+}
+
+
+?>
